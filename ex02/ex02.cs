67b743cb6c5c61724088fec62a51daefb531fcfa
Larksel <lemuelgames@outlook.com>
2024-03-28 22:48:16 -0300

    Exercicios completos

---
+++ ex02/ex02.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+/*
+    2)Crie um programa que preencha uma matriz 2x4 com números inteiros, calcule e mostre:
+    a)A quantidade de elementos entre 12 e 20 em cada linha;
+    b)A média dos elementos pares da matriz.
+*/
+
+class Program
+{
+    static void Main()
+    {
+        int[,] mat = new int[2, 4];
+        Random random = new Random();
+
+        int qntL1 = 0;
+        int qntL2 = 0;
+        int somaPares = 0;
+        int qntPares = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                mat[i, j] = random.Next(0, 31);
+            }
+        }
+
+        Console.WriteLine("Matriz:");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                int num = mat[i, j];
+
+                Console.Write(num + " ");
+
+                if (num % 2 == 0)
+                {
+                    qntPares++;
+                    somaPares += num;
+                }
+
+                if (num >= 12 && num <= 20)
+                {
+                    if (i == 0)
+                    {
+                        qntL1++;
+                    } else
+                    {
+                        qntL2++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        float mediaPares = (float) somaPares / qntPares;
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Quantidade de elementos entre 12 e 20: ");
+        Console.WriteLine("- Linha 1: " + qntL1);
+        Console.WriteLine("- Linha 2: " + qntL2);
+        Console.WriteLine("Media dos elementos pares: " + mediaPares);
+    }
+}
---
+++ ex03/ex03.cs
@@ -0,0 +1,63 @@
+﻿using System;
+/*
+    3) Faça um programa que receba uma matriz 10x10 e calcule a media dos elementos da 
+    diagonal principal e da diagonal secundária. 
+*/
+
+class Program
+{
+    static void Main()
+    {
+        int[,] mat = new int[10, 10];
+        //Random random = new Random();
+
+        int somaDP = 0, somaDS = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write($"Digite o elemento {i} x {j}: ");
+
+                if (!int.TryParse(Console.ReadLine(), out mat[i, j]))
+                {
+                    Console.WriteLine("Valor inválido! Tente novamente");
+                    j--;
+                }
+
+                //mat[i, j] = random.Next(-10, 11);
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Matriz:");
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                int num = mat[i, j];
+                if (i == j)
+                {
+                    somaDP += num;
+                }
+
+                if (i + j == 9)
+                {
+                    somaDS += num;
+                }
+
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        float mediaDP = (float)somaDP / 10;
+        float mediaDS = (float)somaDS / 10;
+
+        Console.WriteLine();
+        Console.WriteLine("Média dos elementos da diagonal principal: " + mediaDP);
+        Console.WriteLine("Média dos elementos da diagonal secundária: " + mediaDS);
+    }
+}
